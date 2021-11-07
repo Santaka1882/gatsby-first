cd7c849fbf722c5d1569abdd8f192a8680e6a4f5
Cristian Buitrago <Crisis1882@gmail.com>
2021-11-06 20:08:41 -0500

    Finished Gatsby tutorial part 2

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About me'>
+      <p>My name is Cris and im making a blog with Gatsby</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
