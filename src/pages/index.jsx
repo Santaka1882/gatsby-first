b67fe00c9fd1b086a7a1b766acc9d57f2253d9b4
Cristian Buitrago <Crisis1882@gmail.com>
2021-11-08 15:31:45 -0500

    Aya Fumo (ᗜˬᗜ)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
-      <p>I'm making this by following the Gatsby Tutorial</p>
+      <p>Aya fumo (ᗜˬᗜ)</p>
+      <StaticImage
+        src='https://i.pinimg.com/564x/ea/4f/2d/ea4f2de19f2e76adafc532edf0401bc4.jpg' 
+        alt='Aya fumo (ᗜˬᗜ)'
+      />
     </Layout>
   )
 }
